ebce207cd20d323bb640fa5055162802a6e95403
Seyithan <seyithan@icloud.com>
2024-07-17 12:49:43 +0200

    Initial commit

---
+++ lib/core_merchant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "core_merchant/version"
+
+module CoreMerchant
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/core_merchant/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CoreMerchant
+  VERSION = "0.1.0"
+end
